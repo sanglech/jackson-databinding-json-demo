150f27ff31fd7b949215ef8fb68e1f424e77a9e7
Christian <christian@sanglech.com>
2021-04-23 12:55:40 +0900

    Added ability to read nested JSON obects

--- src/main/java/com/christian/jackson/json/demo/Driver.java
+++ src/main/java/com/christian/jackson/json/demo/Driver.java
@@ -14,11 +14,23 @@ public class Driver {
 
             //read json file and convert to pojo
             
-            Student theStudent=mapper.readValue(new File("data/sample-lite.json"),Student.class);
+            Student theStudent=mapper.readValue(new File("data/sample-full.json"),Student.class);
 
             //print first name and last name
             System.out.println("Student First name: "+ theStudent.getFirstName());
             System.out.println("Student Last name: "+ theStudent.getLastName());
+            
+            //print out address
+            Address tempAddress= theStudent.getAddress();
+            
+            System.out.println("The street: "+ tempAddress.getStreet());
+            System.out.println("The city: "+ tempAddress.getCity());
+            
+            //print out languages
+            for(String tempLang: theStudent.getLanguages()) {
+            	System.out.println("Languages: "+ tempLang);
+            }
+            
 
         }catch(Exception e){
             e.printStackTrace();
--- src/main/java/com/christian/jackson/json/demo/Student.java
+++ src/main/java/com/christian/jackson/json/demo/Student.java
@@ -5,6 +5,12 @@ public class Student {
     private String firstName;
     private String lastName;
     private boolean active;
+    
+    private Address address;
+    
+    private String [] languages;
+    
+    
     public Student(){
 
     }
@@ -39,6 +45,19 @@ public class Student {
     public void setActive(boolean active) {
         this.active = active;
     }
-
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	public String [] getLanguages() {
+		return languages;
+	}
+	public void setLanguages(String [] languages) {
+		this.languages = languages;
+	}
+    
+    
 
 }
